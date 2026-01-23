8fc99c9ee5375d611940ff0c642f47e200f1ddfc
Arkhianova <arkhianova@gmail.com>
2026-01-23 18:47:25 +0100

    fixed : filtrage realisations + menu deroulant Select

--- src/components/Select/index.jsx
+++ src/components/Select/index.jsx
@@ -15,11 +15,13 @@ const Select = ({
 }) => {
   const [value, setValue] = useState();
   const [collapsed, setCollapsed] = useState(true);
+
   const changeValue = (newValue) => {
-    onChange();
+    onChange(newValue);
     setValue(newValue);
     setCollapsed(newValue);
   };
+
   return (
     <div className={`SelectContainer ${type}`} data-testid="select-testid">
       {label && <div className="label">{label}</div>}
@@ -31,8 +33,8 @@ const Select = ({
           {!collapsed && (
             <>
               {!titleEmpty && (
-                <li onClick={() => changeValue(null)}>
-                  <input defaultChecked={!value} name="selected" type="radio" />{" "}
+                <li onClick={() => changeValue("Toutes")}>
+                  <input defaultChecked={!value || value === "Toutes"} name="selected" type="radio" />{" "}
                   Toutes
                 </li>
               )}
--- src/containers/Events/index.jsx
+++ src/containers/Events/index.jsx
@@ -14,8 +14,8 @@ const EventList = () => {
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
   const filteredEvents = (
-    (!type
-      ? data?.events
+    ((type && type !== "Toutes")
+      ? data?.events.filter((event) => event.type === type)
       : data?.events) || []
   ).filter((event, index) => {
     if (
@@ -43,7 +43,7 @@ const EventList = () => {
           <Select
             selection={Array.from(typeList)}
             onChange={(value) => (value ? changeType(value) : changeType(null))}
-          />
+            />
           <div id="events" className="ListContainer">
             {filteredEvents.map((event) => (
               <Modal key={event.id} Content={<ModalEvent event={event} />}>
